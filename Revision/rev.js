91a6ef4fa63a4e9ecc80c54de277e99a05ad1e57
srishti srivastava <ssrishti508@gmail.com>
2021-12-29 12:20:15 +0530

    Event object

--- Revision/rev.js
+++ Revision/rev.js
@@ -1,1 +1,23 @@
-//replacing elements done
+//Event Listners
+
+// document.querySelector('.clear-tasks').addEventListener('click',function (e) {
+// console.log("hello world");    
+// e.preventDefault();
+// });
+
+document.querySelector('.clear-tasks').addEventListener('click',onclick);
+
+function onclick(e) {
+    // console.log("hello world");    
+    let val;
+    // val = e;
+    
+    val = e.target;
+    val = e.timeStamp;
+    val = e.clientY;
+    val = e.clientX;
+    val = e.offsetY;
+    val = e.offsetX;
+    console.log(val);
+    e.preventDefault();
+    }
