b120fa40e2fb48be4171ce5ecf265aeb394b4129
srishti srivastava <ssrishti508@gmail.com>
2022-01-17 18:43:48 +0530

    Constructor and this keyword

--- Revision/rev.js
+++ Revision/rev.js
@@ -1,22 +1,20 @@
-// localStorage.setItem('name','srishti');
-// sessionStorage.setItem('name','srivastava');
+function Person(name,dob) {
+    this.name = name,
+    this.birthday  = new Date(dob);
+    this.getAge = function () {
+        const diff = Date.now() - this.birthday.getTime();
+        const ageDate = new Date(diff);
+        return Math.abs(ageDate.getUTCFullYear()-1970);
+    }
+    //console.log(this);
+}
 
-// // localStorage.removeItem('name');
+// console.log(this);
+// this.alert("heya");
 
-// const namee = localStorage.getItem('name');
-// console.log(namee);
-// localStorage.clear();
+const brad = new Person("Brad",'9-10-1981');
+//const john = new Person("John",40);
 
-// document.querySelector('form').addEventListener('submit',function (e) {
-//     const task = document.getElementById('task').value;
-//     // console.log(task);
-//     // localStorage.setItem('task',task);
-//     let tasks;
-//     if (localStorage.getItem('tasks')==null) {
-//         tasks = [];
-//     }
-//     else{
-//         tasks = JSON.parse(localStorage.getItem('tasks'));
-//     }
-//     tasks.push(task);
-// });
+console.log(brad);
+console.log(brad.getAge());
+// console.log(john);
