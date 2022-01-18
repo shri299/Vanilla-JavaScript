5aee12f9e7408f67f6e04621abf291d49912521a
srishti srivastava <ssrishti508@gmail.com>
2022-01-18 19:13:23 +0530

    Prototypes

--- Revision/rev.js
+++ Revision/rev.js
@@ -1,1 +1,43 @@
-//see section 5 part 2
+//object.prototytpe
+//person.prototype
+
+function Person(firstName,lastName,dob){  //constructor shd start with capital letters
+    this.firstName = firstName;  //name is the property of the contructor person
+    this.lastName = lastName;
+    this.birthday = new Date(dob);
+    // this.getAge = function(){ //method within a constructor
+    //     const diff = Date.now() - this.birthday.getTime();
+    //     const ageDate = new Date(diff);
+    //     return Math.abs(ageDate.getUTCFullYear() - 1970);
+    // }
+}
+
+//calculate age
+
+Person.prototype.getAge = function(){ //method within a constructor
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+//get full name
+
+Person.prototype.getfullname = function (){
+    return `${this.firstName} ${this.lastName}`
+}
+
+//gets married
+
+Person.prototype.getsMarried = function (newLastName) {
+    this.lastName = newLastName;
+}
+
+const john = new Person('John','Doe','8-12-99');
+const mary = new Person('Mary','Miller','7-12-90');
+
+console.log(mary.getAge());
+console.log(john.getfullname());
+mary.getsMarried('smith');
+console.log(mary.getfullname());
+
+//console.log(mary);
