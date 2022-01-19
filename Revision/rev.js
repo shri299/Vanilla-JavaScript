1f094fa83c0b209d544830b8dd324d3d19ec660e
srishti srivastava <ssrishti508@gmail.com>
2022-01-19 15:02:50 +0530

    Inheritance in ES6

--- Revision/rev.js
+++ Revision/rev.js
@@ -1,16 +1,31 @@
-//ES6
+//Subclasses or inheritance in ES6
 
-class Person {
+class Person
+{
     constructor(firstName,lastName)
     {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-    greeting() {
-        return `Hello ${this.firstName} ${this.lastName}`;
+    greeting(){
+        return `Hello there ${this.firstName} ${this.lastName}`;
     }
 }
 
-const mary = new Person("Mary","Williams");
-console.log(mary);
-console.log(mary.greeting());
+class Customer extends Person{
+    constructor(firstName,lastName,phone)
+    {
+        super(firstName,lastName);//inheriting from parent
+        this.phone = phone;
+    }
+
+    static getMemebershipCost()
+    {
+        return 500;
+    }
+} 
+
+const john = new Customer("John","Doe","12345677");
+console.log(john); 
+console.log(john.greeting());
+console.log(Customer.getMemebershipCost());
