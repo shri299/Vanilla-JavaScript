4c65d9fef379941235a416296dc24861eba880b2
srishti srivastava <ssrishti508@gmail.com>
2022-01-19 14:56:12 +0530

    Classes in ES6

--- Revision/rev.js
+++ Revision/rev.js
@@ -1,29 +1,16 @@
-//Different way to create objects
+//ES6
 
-const personPrototype = {
-    greeting: function () {
-        return "Hello There!"
-    },
-    getsMarried: function (newLastName) {
-        this.lastName = newLastName;
+class Person {
+    constructor(firstName,lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    greeting() {
+        return `Hello ${this.firstName} ${this.lastName}`;
     }
 }
 
-const mary = Object.create(personPrototype);
-mary.firstName = "Mary";
-mary.lastName = "Williams";
-mary.age = 30;
-
+const mary = new Person("Mary","Williams");
 console.log(mary);
 console.log(mary.greeting());
-mary.getsMarried('Thompson');
-
-
-const brad = Object.create(personPrototype,{
-    firstName:{value:"Brad"},
-    lastName:{value:"Traversy"},
-    age:{value:30}
-})
-
-console.log(brad);
-console.log(brad.greeting());
