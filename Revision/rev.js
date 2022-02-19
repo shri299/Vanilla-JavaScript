ce6c26b9b35645af71affe049ad948b33dbda216
srishti srivastava <ssrishti508@gmail.com>
2022-02-19 18:11:15 +0530

    Ajax with json data

--- Revision/rev.js
+++ Revision/rev.js
@@ -1,27 +1,54 @@
-document.getElementById('button').addEventListener('click',loadData);
+document.getElementById('button1').addEventListener('click',loadCustomer);
+document.getElementById('button2').addEventListener('click',loadCustomers);
 
-
-function loadData() {
-    //Create an XHR object
+function loadCustomer(e) {
     const xhr = new XMLHttpRequest();
 
-    //the "open" method
-    xhr.open('GET','revdata.txt',true);
+    xhr.open("GET","rev1.json",true);
 
-    xhr.onprogress = function () {
-        console.log("used as a loader");
-    }
-
-    xhr.onerror = function () {
-        console.log("request error");
-    }
-
-    xhr.onload = function()
-    {
-        if(this.status==200)
+    xhr.onload = function () {
+        if(this.status===200)
         {
-            console.log(this.responseText);
+            //document.getElementById('customer').innerText = this.responseText;
+            const customer = JSON.parse(this.responseText);
+
+            const output = `<ul>
+            <li>ID: ${customer.id}</li>
+            <li>NAME: ${customer.name}</li>
+            <li>COMPANY: ${customer.company}</li>
+            <li>PHONE: ${customer.phone}</li></ul>`
+
+            document.getElementById('customer').innerHTML = output;
         }
     }
+
+    xhr.send();
+}
+
+
+function loadCustomers(e) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("GET","rev2.json",true);
+
+    xhr.onload = function () {
+        if(this.status===200)
+        {
+            //document.getElementById('customer').innerText = this.responseText;
+            const customers = JSON.parse(this.responseText);
+            let output = '';
+            customers.forEach(function(customer){
+                output += `<ul>
+                <li>ID: ${customer.id}</li>
+                <li>NAME: ${customer.name}</li>
+                <li>COMPANY: ${customer.company}</li>
+                <li>PHONE: ${customer.phone}</li></ul>`
+            });
+            
+
+            document.getElementById('Customers').innerHTML = output;
+        }
+    }
+
     xhr.send();
 }
