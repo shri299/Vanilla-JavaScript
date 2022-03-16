9dfbd7594e1d75df721a0d292c87b8e6a3e3dfea
srishti srivastava <ssrishti508@gmail.com>
2022-03-16 17:43:39 +0530

    Arrow Functions

---
+++ Revision/revvv.js
@@ -0,0 +1,22 @@
+// const sayHello = function () {
+//     console.log("say hello");
+// }
+
+// const sayHello =  () => {
+//     console.log("say hello");
+// }
+
+// const sayHello =  () => console.log("say hello");
+
+const whatever = () => "Hmmmm";
+
+const retobjlit = () => ({msg:"heya!!"});
+
+const sayHello =  name => console.log(`Hello ${name}`);
+
+
+sayHello("Srishti");
+
+console.log(whatever());
+
+console.log(retobjlit());
