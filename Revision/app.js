a18bcca621d00821f5e82e1efc4d94283ae61617
Srishti Srivastava <ssrishti508@gmail.com>
2022-04-14 18:49:47 +0530

    async and await

--- Revision/app.js
+++ Revision/app.js
@@ -50,7 +50,9 @@
 // }
 
 
-const http = new EasyHTTP;
+//another lecture 
+
+//const http = new EasyHTTP;
 
 //get users
 
@@ -64,11 +66,11 @@ const http = new EasyHTTP;
 
 //post users
 
-const data = {
-    name :'srishti',
-    email:'abc@gmail.com',
-    username:'srsrivas'
-}
+// const data = {
+//     name :'srishti',
+//     email:'abc@gmail.com',
+//     username:'srsrivas'
+// }
 
 // http.post('https://jsonplaceholder.typicode.com/users',data)
 // .then((data)=>{
@@ -91,13 +93,69 @@ const data = {
 
 //delete request
 
-http.delete('https://jsonplaceholder.typicode.com/users/2')
-.then((data)=>{
+// http.delete('https://jsonplaceholder.typicode.com/users/2')
+// .then((data)=>{
+//     console.log(data);
+// })
+// .catch((err)=>{
+//     console.log(err);
+// });
+
+
+//async and await
+
+// async function myfunc()
+// {
+
+//     const promise = new Promise((resolve,reject)=>{
+//         setTimeout(() => {
+//             resolve("Hello")
+//         }, 2000);
+//     })
+
+//     const error = false;
+//     if (!error) {
+//         const res = await promise;
+//         return res;
+//     }
+//     else{
+//         await Promise.reject(new Error('Something Went Wrong!'));
+
+//     }
+// }
+
+// console.log(1);
+// console.log(2);
+// myfunc().then((data)=>{
+//     console.log(data);
+// }).catch((err)=>{
+//     console.log(err);
+// })
+
+// console.log(3);
+// console.log(4);
+
+
+//using asyn with fetch
+
+
+async function getusers()
+{
+    //await response of fetch call
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+
+    //only proceed once first promise is resolved
+    const data = await response.json();
+
+    //only proceed once second promise is resolved
+    return data;
+}
+
+getusers().then((data)=>{
     console.log(data);
-})
-.catch((err)=>{
+}).catch((err)=>{
     console.log(err);
-});
+})
 
 
 
