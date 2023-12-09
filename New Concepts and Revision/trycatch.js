b532c74dc9ebe3f7e61523eeacd55b000c7d6d95
Srishti Srivastava <ssrishti508@gmail.com>
2023-12-09 21:35:18 +0530

    try catch

---
+++ New Concepts and Revision/trycatch.js
@@ -0,0 +1,18 @@
+// try{
+//     console.log(x);
+// } catch(err){
+//     console.log(err);
+// }
+
+// function double(num){
+//     if(isNaN(num)){
+//         throw new Error("not a number")
+//     }
+//     return num*2;
+// }
+
+// try{
+//     console.log(double("str"));;
+// }catch(err){
+//     console.log(err);
+// }
