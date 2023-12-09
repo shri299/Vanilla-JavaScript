44fd8dffccb4570a1adc806fd87eb52419a03c89
Srishti Srivastava <ssrishti508@gmail.com>
2023-12-09 21:47:28 +0530

    error handling in async await

--- New Concepts and Revision/trycatch.js
+++ New Concepts and Revision/trycatch.js
@@ -16,3 +16,38 @@
 // }catch(err){
 //     console.log(err);
 // }
+
+
+const getData = async ()=>{
+    try {
+        const resObj = await fetch('https://jsonplaceholder.typicode.com/uses');
+        if(!resObj.ok){
+            throw new Error("request failed");
+        }
+        else{
+            const data = await resObj.json();
+            console.log(data);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getData();
+
+
+
+const getPost = async ()=>{
+    const resObj = await fetch('https://jsonplaceholder.typicode.com/uses');
+        if(!resObj.ok){
+            throw new Error("request failed");
+        }
+        else{
+            const data = await resObj.json();
+            console.log(data);
+        }
+}
+
+getPost().catch((err)=>{
+    console.log(err);
+});
